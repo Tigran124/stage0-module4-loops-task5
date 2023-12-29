a2ea35454c0362dd91cd82ab831a75caf6022bfb
Tigran124 <tigran.h.988@gmail.com>
2023-12-29 14:56:05 +0400

    changes completed

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,23 @@
 package school.mjc.stage0.loops.task5;
 
-public class Hourglass {
-    public void printHourglassOfGivenSize(int height) {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+public class Hourglass {
+    public static void main(String[] args) {
+        printHourglassOfGivenSize(5);
+    }
+    public static void printHourglassOfGivenSize(int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i <= j && i + j < height || i >= j && i + j >= height - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
